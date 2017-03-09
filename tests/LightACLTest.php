8a0a92a3cb75d1b10d00cb00f99d78f960cd3628
francescobianco <info.francescobianco@gmail.com>
2017-03-10 00:25:30 +0100

    version 0.0.1

--- tests/LightACLTest.php
+++ tests/LightACLTest.php
@@ -6,14 +6,19 @@ use Javanile\Producer;
 use PHPUnit\Framework\TestCase;
 use Javanile\LightACL\LightACL;
 
-Producer::addPsr4(['Javanile\LightACL\\Tests\\' => __DIR__]);
+Producer::addPsr4([
+    'Javanile\LightACL\\' => __DIR__.'/../src',
+    'Javanile\LightACL\\Tests\\' => __DIR__
+]);
+
+Producer::addPsr4(['' => __DIR__]);
 
 final class LightACLTest extends TestCase
 {
     public function testCliStaticMethod()
     {
         $object = new LightACL();
-        $this->assertInstanceOf('LightACL', $object);
+        $this->assertInstanceOf('Javanile\LightACL\\LightACL', $object);
 
         $output = "Hello World!";
         $this->assertRegexp('/World/i', $output);
